d16bb4cd9868fe076b29c2e2d5e936281c96720b
ChristopherPeralta <cperaltapenaloza@gmail.com>
2024-06-08 19:24:40 -0500

    Category Add handler

--- src/controllers/category.controller.js
+++ src/controllers/category.controller.js
@@ -1,90 +1,64 @@
 const Category = require('../models/category.model');
+const handleDatabaseOperation = require('../middlewares/errorHandler.js');
 
-exports.getAllCategories = async (req, res) => {
-    try {
-        const categories = await Category.findAll();
-        res.json(categories);
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
-};
+exports.getAllCategories = handleDatabaseOperation(async (req, res) => {
+    const categories = await Category.findAll();
+    res.json(categories);
+});
 
-exports.getCategoryById = async (req, res) => {
-    try {
-        const category = await Category.findOne({ 
-            where: { 
-                id: req.params.id 
-            },
-            paranoid: false
-        });
-        if (!category) {
-            return res.status(404).json({ message: 'Category not found' });
-        }
-        if (category.deletedAt) {
-            return res.json({ message: 'This category was deleted', category });
-        }
-        res.json(category);
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
-};
 
-exports.createCategory = async (req, res) => {
-    try {
-        const category = await Category.create(req.body);
-        res.json(category);
-    } catch (error) {
-        res.status(500).json({ message: error.message });
+exports.getCategoryById = handleDatabaseOperation(async (req, res) => {
+    const category = await Category.findOne({ 
+        where: { 
+            id: req.params.id 
+        },
+        paranoid: false
+    });
+    if (!category) {
+        return res.status(404).json({ message: 'Category not found' });
     }
-};
+    if (category.deletedAt) {
+        return res.json({ message: 'This category was deleted', category });
+    }
+    res.json(category);
+});
 
-exports.updateCategory = async (req, res) => {
-    try {
-        const category = await Category.findByPk(req.params.id);
-        if (!category) {
-            return res.status(404).json({ message: 'Category not found' });
-        }
-        await category.update(req.body);
-        res.json(category);
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
-};
+exports.createCategory = handleDatabaseOperation(async (req, res) => {
+    const category = await Category.create(req.body);
+    res.json(category);
+});
 
-exports.deleteCategory = async (req, res) => {
-    try {
-        const category = await Category.findByPk(req.params.id);
-        if (!category) {
-            return res.status(404).json({ message: 'Category not found' });
-        }
-        await category.destroy();
-        res.json({ message: 'Category deleted' });
-    } catch (error) {
-        res.status(500).json({ message: error.message });
+exports.updateCategory = handleDatabaseOperation(async (req, res) => {
+    const category = await Category.findByPk(req.params.id);
+    if (!category) {
+        return res.status(404).json({ message: 'Category not found' });
     }
-};
+    await category.update(req.body);
+    res.json(category);
+});
 
-exports.restoreCategory = async (req, res) => {
-    try {
-        const category = await Category.findOne({ where: { id: req.params.id }, paranoid: false });
-        if (!category || !category.deletedAt) {
-            return res.status(404).json({ message: 'Category not found' });
-        }
-        await category.restore();
-        res.json({ message: 'Category restored', category });
-    } catch (error) {
-        res.status(500).json({ message: error.message });
+exports.deleteCategory = handleDatabaseOperation(async (req, res) => {
+    const category = await Category.findByPk(req.params.id);
+    if (!category) {
+        return res.status(404).json({ message: 'Category not found' });
     }
-};
+    await category.destroy();
+    res.json({ message: 'Category deleted' });
+});
 
-exports.getDeletedCategories = async (req, res) => {
-    try {
-        const categories = await Category.findAll({ 
-            paranoid: false 
-        });
-        const deletedCategories = categories.filter(category => category.deletedAt);
-        res.json(deletedCategories);
-    } catch (error) {
-        res.status(500).json({ message: error.message });
+exports.restoreCategory = handleDatabaseOperation(async (req, res) => {
+    const category = await Category.findOne({ where: { id: req.params.id }, paranoid: false });
+    if (!category || !category.deletedAt) {
+        return res.status(404).json({ message: 'Category not found' });
     }
-};
+    await category.restore();
+    res.json({ message: 'Category restored', category });
+});
+
+exports.getDeletedCategories = handleDatabaseOperation(async (req, res) => {
+    const categories = await Category.findAll({ 
+        paranoid: false 
+    });
+    const deletedCategories = categories.filter(category => category.deletedAt);
+    res.json(deletedCategories);
+});
